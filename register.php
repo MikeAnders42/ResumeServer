5874e66449d5e0fea5bee78ad2c2e440b42b0168
MikeAnders42 <MikeAnders42@gmail.com>
2019-05-24 19:04:56 -0400

    wedding stuf

--- register.php
+++ register.php
@@ -1,26 +1,31 @@
-<?php
-  $key = $POST_['key'];
-  $name = $POST_['name'];
-  $numguests = $POST_['numguests'];
-  $foodpref = $POST_['foodpref'];
-  $allergies = $POST_['allergies'];
 
-  $host_name = 'db5000081171.hosting-data.io';
-  $database = 'dbs75889';
-  $user_name = 'dbu41062';
-  $password = '!Foxfox11';
-  $connect = mysqli_connect($host_name, $user_name, $password, $database);
+<html lang="en">
+  <body>
+    <?php
+      $key = $POST_['key'];
+      $name = $POST_['name'];
+      $numguests = $POST_['numguests'];
+      $foodpref = $POST_['foodpref'];
+      $allergies = $POST_['allergies'];
 
-  if (mysqli_connect_errno()) {
-    echo 'There was a connection error. Try contacting me by <a href="mikeanders42@gmail.com">email</a>.';
-  } else {
+      $host_name = 'db5000081171.hosting-data.io';
+      $database = 'dbs75889';
+      $user_name = 'dbu41062';
+      $password = '!Foxfox11';
+      $connect = mysqli_connect($host_name, $user_name, $password, $database);
 
-    $sql = "UPDATE Registry SET NumGoing = " . $numguests . ", FoodPref = '" . $foodpref . "', Allergies = '" . $allergies . "' WHERE Name = '" . $name . "', ID='" + $key + "'";
-    if ($connect->query($sql) === TRUE) {
-      echo 'Thank you for the RSVP. Wedding details are at the link above.';
-    } else {
-      echo 'This link is invalid or expired. Try contacting me by <a href="mikeanders42@gmail.com">email</a>.';
-    }
-    $conn->close();
-  }
-?>
+      if (mysqli_connect_errno()) {
+        echo 'There was a connection error. Try contacting me by <a href="mikeanders42@gmail.com">email</a>.';
+      } else {
+
+        $sql = "UPDATE Registry SET NumGoing = " . $numguests . ", FoodPref = '" . $foodpref . "', Allergies = '" . $allergies . "' WHERE Name = '" . $name . "', ID='" + $key + "'";
+        if ($connect->query($sql) === TRUE) {
+          echo 'Thank you for the RSVP. Wedding details are at the link above.';
+        } else {
+          echo 'This link is invalid or expired. Try contacting me by <a href="mikeanders42@gmail.com">email</a>.';
+        }
+        $conn->close();
+      }
+    ?>
+  </body>
+</html>
